7bd002eaaa0838c1aebba6526d3e2530120fc148
Andreas Fuchs <asf@boinkor.net>
2024-11-04 10:35:53 -0500

    Fix typing of match criteria

--- asf/app_layout/criteria.py
+++ asf/app_layout/criteria.py
@@ -1,10 +1,11 @@
+from typing import Sequence
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 from talon.ui import Window
 
 class WindowCriteria(ABC):
     @abstractmethod
-    def matching_windows(self, windows: list[Window]) -> list[Window]:
+    def matching_windows(self, windows: Sequence[Window]) -> list[Window]:
         "Return all windows matching the given criteria."
         pass
 
@@ -14,11 +15,11 @@ class NameMatch(WindowCriteria):
 
     title: str
 
-    def matching_windows(self, windows: list[Window]):
+    def matching_windows(self, windows: Sequence[Window]):
         return [win for win in windows if win.title == self.title]
 
 class EmacsMatch(WindowCriteria):
     "Matches emacs windows that have a title bar (i.e., no posframes)"
 
-    def matching_windows(self, windows: list[Window]) -> list[Window]:
+    def matching_windows(self, windows: Sequence[Window]) -> list[Window]:
         return [win for win in windows if win.element.get('AXTitleUIElement', None) is not None]
